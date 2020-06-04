51d085aad21db46f1cf0ff73ba833b1764df926c
Noah Gillard <noah.gillard@student.kdg.be>
2020-06-04 23:23:08 +0200

    Chart display work

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -1,23 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <head>
+        <title>Buy</title>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+
+    </head>
+
+
+
+
+<body>
+<div class="container-fluid" >
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+            <div class="chart-container" style="position: absolute; height:20vh; width:40vw">
+                <canvas id="myChart"></canvas>
             </div>
-        </div>
     </div>
+
 </div>
+
+<script>
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['01/05', '02/05', '03/05', '04/05', '05/05'],
+
+
+
+
+            datasets: [
+                {
+                    label: 'Prijs',
+                    data: [5, 10, 15, 4, 9],
+                    backgroundColor: 'transparent',
+                    borderColor: '#b20000',
+                    borderWidth: 5,
+                    type:'line',
+                },
+
+                {
+                label: 'Stock',
+                data: [12, 19, 3, 10, 6],
+                backgroundColor: '#4c4cff',
+                borderWidth: 1
+                }
+
+            ]
+
+
+
+
+
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        },
+
+
+    }
+
+
+
+
+
+
+
+    );
+
+
+</script>
+
+
+</body>
 @endsection
