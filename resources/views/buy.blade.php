3b3e4baa65d4184b052a1eb3e086b84a4e0b2379
Noah Gillard <noah.gillard@student.kdg.be>
2020-06-05 01:38:36 +0200

    checkboxen add

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -6,18 +6,156 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
-    </head>
 
+    </head>
 
 
 
 <body>
 <div class="container-fluid" >
-    <div class="row justify-content-center">
 
-            <div class="chart-container" style="position: relative; height:30vh; width:60vw">
+    <div class="row">
+        <div class="col" align="center">
+            <div class="chart-container" style="position: relative; height:30vh; width:60vw;display: inline-grid;">
                 <canvas id="myChart"></canvas>
             </div>
+        </div>
+    </div>
+
+    <div class="row mt-5" align="right">
+                <div class="col-2">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Chirurgische maskers</label>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Handschoenen Kort</label>
+                    </div>
+                    <select class="form-control" style="width: 15%;">
+                        <option>XL</option>
+                    </select>
+                </div>
+                <div class="col-2">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Ontsmettings alcohol +70%</label>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Immonium +20%</label>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Beademings mondmasker 100%</label>
+                    </div>
+                </div>
+
+    </div>
+
+    <div class="row mt-5" align="right">
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">FFP2 Maskers</label>
+            </div>
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Ontsmettings alcohol +90%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Immonium 50%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Beademings neusbril</label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row mt-5" align="right">
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Handschoenen Lang</label>
+            </div>
+            <select class="form-control" style="width: 15%;">
+                <option>XL</option>
+            </select>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Ontsmettings handgel +40%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Immonium 75%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Beschermings schort</label>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row mt-5" align="right">
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Spat maskers</label>
+            </div>
+        </div>
+        <div class="col-3">
+
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Ontsmettings handgel +50%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Immonium 100%</label>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Brancard beschermlakens</label>
+            </div>
+        </div>
+
     </div>
 
 </div>
@@ -99,7 +237,6 @@
 
 
     });
-
 
 </script>
 
